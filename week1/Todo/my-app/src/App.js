5f6fbc534c4e4221ec3090e3144d0bf0ce387972
Samantha <samccollinsfl@gmail.com>
2019-12-09 11:42:42 -0500

    FSW-120 Week 1 Todo

---
+++ week1/Todo/my-app/src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+//import logo from './logo.svg';
+//import './App.css';
+
+
+function App() {
+  return (
+      <div>
+        <h1>My To-Do List</h1>
+          <input type= "checkbox" /><span>Homework</span>
+
+          <br></br>
+
+          <input type= "checkbox" /><span>Laundry</span>
+          
+          <br></br>
+
+          <input type= "checkbox" /><span>Feed pets</span>
+
+          <br></br>
+
+          <input type= "checkbox" /><span>Walk dog</span>
+      </div>
+  )
+
+}
+
+export default App;
